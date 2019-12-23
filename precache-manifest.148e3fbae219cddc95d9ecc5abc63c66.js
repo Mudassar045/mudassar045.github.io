3188ecafcd8b88a6de124dfae9273f4c26731cb9
Mudassar045 <mudassaralics007@gmail.com>
2019-12-23 05:37:33 +0500

    Updates

---
+++ precache-manifest.148e3fbae219cddc95d9ecc5abc63c66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b12664a911ef66566d7257f30dec5d",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "2b468695c00e7224621f",
+    "url": "/blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ff49338cea201cde0deb",
+    "url": "/blog/static/js/2.2fe1c9d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blog/static/js/2.2fe1c9d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b468695c00e7224621f",
+    "url": "/blog/static/js/main.47d16a53.chunk.js"
+  },
+  {
+    "revision": "6e85bb4801445d809ead",
+    "url": "/blog/static/js/runtime-main.dc915a2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
