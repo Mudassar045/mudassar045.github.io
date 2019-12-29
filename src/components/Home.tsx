74fc9fdfde7c0c6fbb482ed8dd41ffa0617f0025
Mudassar045 <mudassaralics007@gmail.com>
2019-12-29 23:20:18 +0500

    add Fontawesome icons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 import SiteConfig from './../constants/SiteConfig'
 import Avatar from './../assets/mudassar-ali.jpg'
 
@@ -20,36 +23,23 @@ const Home = () => {
                             <p className="has-text-centered">
                                 <a className="button is-large is-white" href={SiteConfig.blog}>
                                     <span className="icon">
-                                        <i className="fa fa-pencil"></i>
+                                        <FontAwesomeIcon icon={faCoffee} />
                                     </span>
                                     <span>Writings</span>
                                 </a>
                                 <a className="button is-large is-white" href={SiteConfig.userGitHub}>
                                     <span className="icon">
-                                        <i className="fa fa-github"></i>
+                                        <FontAwesomeIcon icon={faGithub} />
                                     </span>
                                     <span>Code</span>
                                 </a>
                                 <a className="button is-large is-white" href={SiteConfig.userTwitter}>
                                     <span className="icon">
-                                        <i className="fa fa-twitter"></i>
+                                        <FontAwesomeIcon icon={faTwitter} />
                                     </span>
                                     <span>Tweets</span>
                                 </a>
                             </p>
-                            <br/>
-                            <a href="/thanks">Huge thanks to all my amazing supporters!</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="hero-foot">
-                    <div className="container">
-                        <div className="tabs is-centered">
-                            <ul>
-                                <li>
-                                    <i id="scroll-hint" className="fa fa-caret-down" aria-hidden="true"></i>
-                                </li>
-                            </ul>
                         </div>
                     </div>
                 </div>
