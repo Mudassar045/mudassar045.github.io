0b6c4611622089d8d6b029c6d6092d85cb084ee3
Mudassar045 <mudassaralics007@gmail.com>
2019-12-29 23:56:50 +0500

    add about component

---
+++ src/components/About.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import SiteConfig from './../constants/SiteConfig'
+
+const About = () => {
+    return(<section className="hero is-fullheight is-default">
+        <div className="hero-body">
+            <div className="container">
+                <div className="columns is-vcentered is-centered is-text-centered">
+                    <div className="column is-half is-narrow content">
+                        <h1 id="about-sindre-sorhus">About {SiteConfig.userName}</h1>
+                        <p>I’m a Full-Stack software developer at <a href="https://labs.cerp.org.pk">CERP Pakistan</a>. I started, my professional journey back in July, 2019.
+                         I'm an avid participant in several local and international open source projects on GitHub.</p>
+                        <h3 id="more">More</h3>
+                        <ul>
+                            <li><a href="https://mudassarali.com/articles/about-me">About Me Blog Post</a></li>
+                        </ul>
+                        <h3 id="links">Links</h3>
+                        <ul>
+                            <li><a href="https://github.com/mudassar045">GitHub</a></li>
+                            <li><a href="https://www.linkedin.com/in/mudassar-ali-045/">LinkedIn</a></li>
+                            <li><a href="https://twitter.com/voidwebdev">Twitter</a></li>
+                            <li><a href="https://web.facebook.com/mudassar045">Facebook</a></li>
+                            <li><a href="https://instagram.com/voidwebdev">Instagram</a></li>
+                            <li><a href="/contact">Email</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>);
+}
+
+export default About
