35958869b670c10831f7335f07a011134d634ce6
Mudassar045 <mudassaralics007@gmail.com>
2020-01-01 00:38:02 +0500

    add layout componet

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import config from '../constants/SiteConfig'
+import Navigation from '../components/Navigation'
+
+type PropsType = {
+    children: any
+}
+
+const Layout = ({ children }: PropsType) => {
+
+    return(
+        <>
+            <Navigation menuLinks={ config.menuLinks }/>
+            { children }
+        </>
+    );
+}
+
+export default Layout
