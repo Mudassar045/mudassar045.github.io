4601c1e1f5daf8f72d4406ffd5736d52a5907934
Mudassar045 <mudassaralics007@gmail.com>
2019-12-31 23:59:46 +0500

    add contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Layout from './../layout/Layout'
+import config from './../constants/SiteConfig'
 
 const Contact = () => {
-    return(
-        <>
-        </>
-    );
+    return(<Layout>
+        <section className="hero is-fullheight is-default">
+            <div className="hero-body">
+                <div className="container">
+                    <div className="columns is-vcentered is-centered is-text-centered" style={{marginBottom: 180}}>
+                        <div className="column is-half is-narrow content">
+                            <h1 id="contact">Contact</h1>
+                            <p>I value my time, so make it worth, and <a href="https://www.google.com/search?q=succinct+emails">keep it succinct</a>.</p>
+                            <hr/>
+                            <p id="email" style={{fontSize: 18, textAlign: "center"}}>
+                                <a href={`mailto:${config.userEmail}`}>{config.userEmail}</a>
+                            </p>
+                            <hr/>
+                            <p>Please no support questions, job offers, questionnaires, etc.</p>
+                            <p>If the answer to your question could be interesting to others, consider asking in my <a href={`${config.userGitHub}/ama`}>AMA</a> instead.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </Layout>);
 }
 
 export default Contact
