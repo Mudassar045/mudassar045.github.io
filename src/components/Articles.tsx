126ecece1f2a82ff781dd95fa78e6a88d4436644
Mudassar045 <mudassaralics007@gmail.com>
2020-01-01 16:00:31 +0500

    add article component

---
+++ src/components/Articles.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Layout from '../layout/Layout'
+
+class Article extends Component {
+
+    render() {
+
+    return(
+        <Layout>
+            <section className="hero">
+                <div className="hero-body">
+                    <div className="container">
+                        <div className="columns is-vcentered is-centered is-text-centered">
+                            <div className="column is-half is-narrow content">
+                                <h1>Coming soon...</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Layout>
+        );
+    }
+}
+
+export default Article
