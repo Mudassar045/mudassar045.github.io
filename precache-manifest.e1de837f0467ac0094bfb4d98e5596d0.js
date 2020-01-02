25022b6a3804b53a85daed1b2bc5e2edac8ace7b
Mudassar045 <mudassaralics007@gmail.com>
2020-01-02 12:54:48 +0500

    Updates

---
+++ precache-manifest.e1de837f0467ac0094bfb4d98e5596d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2450efaf079c6f43b097658f2cda090",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "16e9ebae13790d24ed44",
+    "url": "/blog/static/css/main.f5d9f16f.chunk.css"
+  },
+  {
+    "revision": "9f54e1e5c1d22f0a8784",
+    "url": "/blog/static/js/2.543a5f99.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/blog/static/js/2.543a5f99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16e9ebae13790d24ed44",
+    "url": "/blog/static/js/main.950a367c.chunk.js"
+  },
+  {
+    "revision": "ab0a3417dc239f04ba10",
+    "url": "/blog/static/js/runtime-main.95a90ade.js"
+  },
+  {
+    "revision": "62b7f00c913723d1930426b16a148b65",
+    "url": "/blog/static/media/cool-cat.62b7f00c.png"
+  },
+  {
+    "revision": "4d96b4309e46c6639f34716ddde80c8c",
+    "url": "/blog/static/media/mudassar-ali.4d96b430.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efb7eaa85e47f0abd03d6cb151344e06.js"
+  "/blog/precache-manifest.e1de837f0467ac0094bfb4d98e5596d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
