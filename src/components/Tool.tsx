ad0cfb2bd7c85bbdc54196490b8d7876901a3865
Mudassar045 <mudassaralics007@gmail.com>
2020-01-28 00:21:48 +0500

    add tools

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -11,14 +11,19 @@ const Tool = () => {
                             <div className="column is-half is-narrow content">
                                 <h1 id="contact">What Tools I use</h1>
                                 <p>Here's a list of all the tools I use. If I missed a category, just contact me and I'll make sure to add it.</p>
-
                                 <h3 id="links">Code Tips</h3>
                                 <ul>
-                                    
+                                    <li><a href="https://carbon.now.sh/">Carbon</a></li>
+                                    <li><a href="https://getemoji.com/">Get Emoji</a></li>
+                                    <li><a href="https://canva.com/">Canva</a></li>
+                                    <li><a href="https://www.microsoft.com/en-pk/p/paint-3d/9nblggh5fv99?activetab=pivot:overviewtab/">MS Paint</a></li>
                                 </ul>
                                 <h3 id="links">Computer</h3>
+                                <p>These are my programming hardware and software.</p>
                                 <ul>
-                                    
+                                    <li><a href="https://www.lenovo.com/pk/en/laptops/thinkpad/thinkpad-e-series/ThinkPad-E580/p/22TP2TEE580">Lenovo E580 with Linux Ubuntu</a></li>
+                                    <li><a href="https://code.visualstudio.com/">VS Code Editor</a></li>
+                                    <li><a href="https://github.com/mudassar045">Github</a></li>
                                 </ul>
                             </div>
                         </div>
