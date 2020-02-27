76226f25e55f775315ce5978e763b715d68c535d
Mudassar045 <mudassaralics007@gmail.com>
2020-02-27 11:14:08 +0500

    apply react-motion TransitionMotion component

--- src/modules/work/EmojiExplorer/EmojiExplorer.tsx
+++ src/modules/work/EmojiExplorer/EmojiExplorer.tsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react'
 import Layout from 'layout/Layout'
 import { getEmojis, filterEmoji } from 'modules/work/EmojiExplorer/EmojiList'
+import { spring, TransitionMotion, presets } from 'react-motion'
 
 const EmojiExplorer = () => {
 
 	const random_emojis = getEmojis(50)
+		.map((curr) => {
+			return {
+				key: curr.title,
+				data: curr
+			}
+		})
 	const [emojis, setEmojis] = useState(random_emojis)
 
 	const onChangeInput = (searchText: string) => {
-		setEmojis(filterEmoji(searchText, 50))
+
+		const filtered_emojis = filterEmoji(searchText, 50)
+			.map((curr) => {
+				return {
+					key: curr.title,
+					data: curr
+				}
+			})
+		setEmojis(filtered_emojis)
 	}
 
 	const emojiImageSrc = (symbol: any): string => {
@@ -16,6 +31,37 @@ const EmojiExplorer = () => {
 		const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`
 
 		return src
+	}
+
+	// actual animation-related logic
+	const getDefaultStyles = () => {
+		return emojis.map(emoji => ({ ...emoji, style: { height: 0, opacity: 1 } }))
+	}
+
+	const getStyles = () => {
+		return emojis.map(emoji => {
+			return {
+				...emoji,
+				style: {
+					height: spring(60, presets.gentle),
+					opacity: spring(1, presets.gentle),
+				}
+			}
+		})
+	}
+
+	const willEnter = (): any => {
+		return {
+			height: 0,
+			opacity: 1,
+		}
+	}
+
+	const willLeave = (): any => {
+		return {
+			height: spring(0),
+			opacity: spring(0),
+		}
 	}
 
 	return (
@@ -26,27 +72,33 @@ const EmojiExplorer = () => {
 						<div className="columns is-vcentered is-centered is-text-centered">
 							<div className="column content">
 								<h3>Emoji Explorer - Find Emoji</h3>
-								<p></p>
 								<div className="box">
 									<input className="input" placeholder="Paste Emoji or Search e.g. Smile" onChange={(e) => onChangeInput(e.target.value)} />
 									<table className="table" style={{ marginTop: 10 }}>
-										<tbody>
+										<TransitionMotion
+											styles={getStyles()}
+											defaultStyles={getDefaultStyles()}
+											willEnter={willEnter}
+											willLeave={willLeave}>
 											{
-												emojis
-													.sort((a, b) => a.title.localeCompare(b.title))
-													.map(emoji => <tr key={emoji.title}>
-														<td>
-															<div>
-																<figure>
-																	<img src={emojiImageSrc(emoji.symbol)} height={24} width={24} alt={emoji.title} />
-																</figure>
-																<h3 className="title is-capitalized">{emoji.title}</h3>
-																<p className="subtitle is-6">{emoji.keywords.split(' ').join(', ').toString()}</p>
-															</div>
-														</td>
-													</tr>)
+												styles =>
+													<tbody>
+														{
+															styles.map(({ key, style, data: { symbol, title, keywords } }) => <tr key={key} style={style}>
+																<td>
+																	<div>
+																		<figure>
+																			<img src={emojiImageSrc(symbol)} height={24} width={24} alt={title} />
+																		</figure>
+																		<h3 className="title is-capitalized">{title}</h3>
+																		<p className="subtitle is-6">{keywords.split(' ').join(', ').toString()}</p>
+																	</div>
+																</td>
+															</tr>)
+														}
+													</tbody>
 											}
-										</tbody>
+										</TransitionMotion>
 									</table>
 								</div>
 							</div>
